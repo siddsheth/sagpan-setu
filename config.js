9a76af5eb317a0bf6ab8aafe3bb69b7858dbb9dd
Siddharth Sheth <162272167+siddsheth@users.noreply.github.com>
2026-07-01 07:51:50 +0530

    Replace Supabase config with Firebase config

--- config.js
+++ config.js
@@ -1,7 +1,11 @@
-// સગપણ સેતુ Supabase API કન્ફિગરેશન
-// જો અહીં સાચા ક્રેડિટન્શિયલ ઉમેરવામાં આવશે તો વેબસાઇટ ઓટોમેટિકલી લાઇવ Supabase ડેટાબેઝ સાથે જોડાઈ જશે.
+// સગપણ સેતુ Firebase API કન્ફિગરેશન
+// જો અહીં સાચા ક્રેડિટન્શિયલ ઉમેરવામાં આવશે તો વેબસાઇટ ઓટોમેટિકલી લાઇવ Firebase ડેટાબેઝ અને સ્ટોરેજ સાથે જોડાઈ જશે.
 // જો આ કી ખાલી રાખવામાં આવે તો પોર્ટલ "ઑફલાઇન મોડ" (LocalStorage) માં ચાલશે.
-const SUPABASE_CONFIG = {
-    URL: "https://kztkypkxkiolrlwdswip.supabase.co",       // તમારો Supabase Project URL અહીં દાખલ કરો (દા.ત. https://xxxx.supabase.co)
-    ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt6dGt5cGt4a2lvbHJsd2Rzd2lwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA5MjY3NTksImV4cCI6MjA5NjUwMjc1OX0.H9S6K6gFKuF77RqaMTZjqq0MjC0uGBvnyVJRtDq6pcs"   // તમારી Supabase Public Anon Key અહીં દાખલ કરો
+const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyD95DiTW3q5uYJkdM5aopWWbRIUb4XWiN8",
+    authDomain: "sagpan-setu.firebaseapp.com",
+    projectId: "sagpan-setu",
+    storageBucket: "sagpan-setu.firebasestorage.app",
+    messagingSenderId: "191714603908",
+    appId: "1:191714603908:web:795fcb0ec2eba41f5d63d1"
 };
